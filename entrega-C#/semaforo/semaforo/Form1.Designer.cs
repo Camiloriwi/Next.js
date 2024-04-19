b66f26727d0dd7c889fcc43080f407246e4fac89
Camiloriwi <154547170+Camiloriwi@users.noreply.github.com>
2024-04-19 06:51:52 -0500

    file traffic light

---
+++ entrega-C#/semaforo/semaforo/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace semaforo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.verde = new System.Windows.Forms.PictureBox();
+            this.amarillo = new System.Windows.Forms.PictureBox();
+            this.rojo = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rojop = new System.Windows.Forms.PictureBox();
+            this.verdep = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verde)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.amarillo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rojo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rojop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verdep)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 4800;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 4800;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.pictureBox2.Image = global::semaforo.Properties.Resources._04_semaforo_peatones1;
+            this.pictureBox2.Location = new System.Drawing.Point(70, 133);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(313, 411);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // verde
+            // 
+            this.verde.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.verde.Location = new System.Drawing.Point(727, 402);
+            this.verde.Name = "verde";
+            this.verde.Size = new System.Drawing.Size(149, 142);
+            this.verde.TabIndex = 3;
+            this.verde.TabStop = false;
+            // 
+            // amarillo
+            // 
+            this.amarillo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.amarillo.Location = new System.Drawing.Point(727, 274);
+            this.amarillo.Name = "amarillo";
+            this.amarillo.Size = new System.Drawing.Size(149, 131);
+            this.amarillo.TabIndex = 2;
+            this.amarillo.TabStop = false;
+            // 
+            // rojo
+            // 
+            this.rojo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.rojo.Location = new System.Drawing.Point(727, 157);
+            this.rojo.Name = "rojo";
+            this.rojo.Size = new System.Drawing.Size(149, 124);
+            this.rojo.TabIndex = 1;
+            this.rojo.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.pictureBox1.Image = global::semaforo.Properties.Resources._8bed5d8b91794ba4cf35c0eb6653b13a;
+            this.pictureBox1.Location = new System.Drawing.Point(643, 133);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(313, 411);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rojop
+            // 
+            this.rojop.BackColor = System.Drawing.SystemColors.ControlText;
+            this.rojop.Location = new System.Drawing.Point(144, 168);
+            this.rojop.Name = "rojop";
+            this.rojop.Size = new System.Drawing.Size(166, 170);
+            this.rojop.TabIndex = 5;
+            this.rojop.TabStop = false;
+            // 
+            // verdep
+            // 
+            this.verdep.BackColor = System.Drawing.Color.Black;
+            this.verdep.Location = new System.Drawing.Point(144, 344);
+            this.verdep.Name = "verdep";
+            this.verdep.Size = new System.Drawing.Size(166, 170);
+            this.verdep.TabIndex = 6;
+            this.verdep.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = global::semaforo.Properties.Resources.R;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1035, 650);
+            this.Controls.Add(this.verdep);
+            this.Controls.Add(this.rojop);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.verde);
+            this.Controls.Add(this.amarillo);
+            this.Controls.Add(this.rojo);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "semaforo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verde)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.amarillo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rojo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rojop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verdep)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox rojo;
+        private System.Windows.Forms.PictureBox amarillo;
+        private System.Windows.Forms.PictureBox verde;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox rojop;
+        private System.Windows.Forms.PictureBox verdep;
+    }
+}
+
---
+++ entrega-C#/semaforo/semaforo/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace semaforo
+{
+    public partial class Form1 : Form
+    {
+      
+        private Timer timerParpadeo = new Timer();
+        private int contadorParpadeos = 0;
+        private Timer timerFijo = new Timer();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timerFijo.Interval = 5000;
+            timerFijo.Tick += TimerFijo_Tick;
+           
+            timerParpadeo.Interval = 500; // Intervalo para el parpadeo (500ms)
+            timerParpadeo.Tick += TimerParpadeo_Tick;
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //timer1.Start();
+            rojo.Visible = true;
+            timerFijo.Start();
+        }
+
+        private void TimerFijo_Tick(object sender, EventArgs e)
+        {
+            timerFijo.Stop(); // Detiene el timer fijo
+           // Comienza el parpadeo
+            timerParpadeo.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timerParpadeo.Stop();
+            timer1.Stop();
+            
+            Task.Delay(1000).ContinueWith(t =>
+            {
+                timerFijo.Start();
+                // Continuar con el siguiente estado del semáforo
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        
+            
+        }
+
+        private void TimerParpadeo_Tick(object sender, EventArgs e)
+        {
+            verde.Visible = !verde.Visible; // Parpadea el semáforo en rojo
+            verdep.Visible = !verdep.Visible;
+            contadorParpadeos++;
+            rojop.Visible = true;
+
+            if (contadorParpadeos >= 6) // Después de 3 parpadeos (6 cambios de visibilidad)
+            {
+                rojo.Visible = false; 
+                timerParpadeo.Stop();
+                verde.Visible = true;
+                verdep.Visible = false;
+                timer2.Start(); // Continuar con el siguiente estado del semáforo
+                contadorParpadeos = 0; // Restablecer el contador para el próximo ciclo
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+           
+            rojo.Visible = true;
+            amarillo.Visible = false;
+            rojop.Visible = false;
+            verdep.Visible = true;
+            timer3.Start();
+            timer2.Stop();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            amarillo.Visible = true;
+            verde.Visible = false;
+            rojop.Visible = false;
+            timer1.Start();
+            verdep.Visible = true; 
+            timer3.Stop();
+        }
+    }
+}
+
